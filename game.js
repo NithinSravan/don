b9a49a3d543fed20c1cee7800c8ab3883aa2ba41
Shrihari10 <62280755+Shrihari10@users.noreply.github.com>
2020-04-19 16:27:09 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
+var numArray= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40];
 var container = document.getElementsByClassName("container")[0];
+
 
 for(var i=0;i<20;i++)
 {
@@ -7,3 +9,22 @@ for(var i=0;i<20;i++)
     myDiv.classList.add("box");
     console.log(myDiv);
 }
+numberDisplay();
+function numberDisplay(){
+//function to display numbers on grid
+for(var i=0;i<20;i++)
+    var num= document.getElementsByClassName("box")[i];
+    var para =document.createElement("p");
+    var text =document.createTextNode(numArray[i+1]);
+    para.appendChild(text);
+    num.appendChild(para);
+}
+
+function numberChange(){
+//function to change numbers on grid
+}
+
+const box =document.querySelector("box");
+
+box.addEventListener('click',numberChange);
+
