400894d212cbf333aaf716039a148f0fe8d988fa
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2020-04-20 13:47:16 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var numArray =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 var time = 3;
 var container = document.getElementById("container");
+ const countBox=document.createElement('div');
+ container.appendChild(countBox);
     var count=1;
     var text;
     countDown();
@@ -9,7 +11,6 @@ var container = document.getElementById("container");
         const countBox=document.createElement('div');
         container.appendChild(countBox);
         countBox.innerHTML=time+"...";
-        container.removeChild(countBox);
         time--;
         if (time==0)
             stopCountDown();
