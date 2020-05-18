c13f760e3c15cca127a67bd15336989932c3ff67
Shrihari10 <62280755+Shrihari10@users.noreply.github.com>
2020-05-18 22:30:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-let canvas = document.getElementById("canvas");
-let ctx= canvas.getContext("2d");
-let render;
-let obstacle; 
-let player;
-let pixel;
-let maxScore=0;
-let count=0;
-let n;
-let pauseScreen;
-let resumeImage;
-let reloadImage;
+var canvas = document.getElementById("canvas");
 
-let color = ["#ffffff","#00A4CCFF","#000000","#CB1E04"];
+var ctx = canvas.getContext("2d");
+let render; 
+let stop;
+let stopTimer;
+let timer = 10;
+let time = 1500;
+let index = 0;
+let balls = [];
 
-let newObstacley ;
-let gamePaused = false;
-let best = JSON.parse(localStorage.getItem('bestscore'));
-let jumpSound = new Audio();
+let canvasArea;
+let ballArea = 0;
+
 let burstSound = new Audio();
-jumpSound.src = "jump.wav";
 burstSound.src = "burst.flac";
+
+let pauseScreen = document.getElementById('pausescreen');
+
+let resumeImage;
+resumeImage = document.createElement("img");
+resumeImage.setAttribute("id","resume");
+resumeImage.src = "play.svg";
+
+let reloadImage;
+reloadImage = document.createElement("img");
+reloadImage.setAttribute("id","reload");
+reloadImage.src = "reload.svg";
 
 
 let pauseImage = document.createElement("img");
@@ -28,7 +34,12 @@ let pauseButton = document.getElementById("pause");
 pauseButton.appendChild(pauseImage);
 pauseButton.addEventListener('click',pauseGame);
 
-canvas.addEventListener('click',jumpAudio);
+let gameEnd = document.getElementById("gameover");
+gameEnd.appendChild(reloadImage);
+
+canvas.addEventListener('click', burst);
+
+let best = JSON.parse(localStorage.getItem('maxscore'));
 
 var score = document.createElement("div");
 score.setAttribute("id","score");
@@ -40,23 +51,18 @@ highScore.setAttribute("id","highscore");
 document.getElementsByTagName("body")[0].appendChild(highScore);
 
     if(best == null)
-       { localStorage.setItem('bestscore','0');
+       { localStorage.setItem('maxscore','0');
         highScore.innerHTML = "Best Score : 0";
         } 
     else
-    {    localStorage.setItem('bestscore',JSON.stringify(best));
+    {    localStorage.setItem('maxscore',JSON.stringify(best));
         highScore.innerHTML = "Best Score : " + best;
     }
-
-canvas.addEventListener('click', play );
-canvas.addEventListener('click', function (){
-    player.dy = 5;
-});
 
 window.onopen = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    setup ();
+    setup();
 }
 
 window.onresize = function () {
@@ -68,7 +74,85 @@ window.onresize = function () {
 window.onload = function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    setup ();
+    setup();
+}
+
+
+function rotate( xvelocity, yvelocity, angle) {
+    
+    const rotatedVelocity = {
+        x: xvelocity * Math.cos(angle) - yvelocity * Math.sin(angle),
+        y: xvelocity * Math.sin(angle) + yvelocity * Math.cos(angle)
+    };
+
+    return rotatedVelocity;
+}
+
+function resolveCollision(ball1, ball2) {
+    const dxDiff = ball1.dx - ball2.dx;
+    const dyDiff = ball1.dy - ball2.dy;
+
+    const xDiff = ball2.x - ball1.x;
+    const yDiff = ball2.y - ball1.y;
+
+    if (dxDiff * xDiff + dyDiff * dyDiff >= 0) {
+
+        const angle = -Math.atan2(ball2.y - ball1.y, ball2.x - ball1.x);
+
+        const m1 = ball1.mass;
+        const m2 = ball2.mass;
+
+        const u1 = rotate(ball1.dx,balls.dy, angle);
+        const u2 = rotate(ball2.dx,ball2.dy, angle);
+
+        const v1 = {
+                    x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
+                    y: u1.y 
+                    };
+        const v2 = { 
+                    x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), 
+                    y: u2.y 
+                    };
+
+        const velocity1 = rotate( v1.x, v1.y, -angle);
+        const velocity2 = rotate( v2.x, v2.y, -angle);
+
+        ball1.dx = velocity1.x;
+        ball2.dy = velocity1.y;
+
+        ball2.dx = velocity2.x;
+        ball2.dy = velocity2.y;
+    }
+}
+
+function distance(x1,y1,x2,y2) {
+    let xDistance = x1 - x2;
+    let yDistance = y1 - y2;
+
+    return Math.sqrt(Math.pow(xDistance,2) + Math.pow(yDistance,2));
+
+}
+
+function areaCheck(){
+    canvasArea = canvas.height*canvas.width;
+    for(var i=0;i<balls.length;i++)
+    ballArea += Math.PI*balls[i].r*balls[i].r;
+
+    stopTimer = setInterval(function(){
+        if(ballArea >= 0.45*canvasArea)
+        {
+            timer--;
+            document.getElementById("countdown").innerText = timer;
+        }
+        else 
+        {
+            document.getElementById("countdown").innerHTML = "";
+            timer = 10;
+            clearInterval(stopTimer);
+            if(timer<=0)
+                clearInterval(render);
+        }
+    },1000);
 }
 
 function dispScore(max){
@@ -77,240 +161,185 @@ function dispScore(max){
 }
 
 function dispBestScore(max){
-    best = JSON.parse(localStorage.getItem('bestscore'));
+    best = JSON.parse(localStorage.getItem('maxscore'));
     if(max>best)
     {
-    localStorage.setItem('bestscore',JSON.stringify(max));
+    localStorage.setItem('maxscore',JSON.stringify(max));
     highScore.innerHTML ="Best Score : " + max;
     }
 }
 
-function jumpAudio(){
-        jumpSound.play();
-}
-
 function burstAudio() {
-        burstSound.play();
+    burstSound.play();
 }
 
-function pauseGame(){
-    console.log(1);
-    if(!gamePaused)
-     {  
-        gamePaused = true;
-        clearInterval(render);
-        canvas.removeEventListener('click',jumpAudio);
-        pauseButton.removeEventListener('click',pauseGame);
+function gameEndScreen(max) {
 
-        pauseScreen = document.getElementById('pausescreen');
-
-        resumeImage = document.createElement("img");
-        resumeImage.setAttribute("id","resume");
-        resumeImage.src = "play.svg";
-        pauseScreen.appendChild(resumeImage);
-        reloadImage = document.createElement("img");
-        reloadImage.setAttribute("id","reload");
-        reloadImage.src = "reload.svg";
-        pauseScreen.appendChild(reloadImage);
-
-        pauseScreen.style.display = "block";
-
-        resumeImage.addEventListener('click',function(){
-            player.draw();
-            play();
-            pauseScreen.style.display = "none";
-        })
-
-        reloadImage.addEventListener('click',function(){
-            setup();
-            play();
-            pauseScreen.style.display = "none";
-        })
-
-     }
-     else if(gamePaused){
-        canvas.addEventListener('click',jumpAudio);
-        pauseButton.addEventListener('click',pauseGame);
-        gamePaused = false;
-        play();
-    //     render = setInterval(function(){
-    //         ctx.clearRect(0,0,canvas.width,canvas.height);
-    //         player.jump();
-    //     for(let i=0;i<obstacle.length;i++)
-    //     {    obstacle[i].rotate();
-    //             obstacle[i].burst();
-    //     }
-    // },16);
-    }
+    gameEnd.innerText = "SCORE : " + max; 
+    gameEnd.appendChild(reloadImage);
+    gameEnd.style.display = "block";
+    reloadImage.addEventListener('click',reload);
 }
 
-function  setup (){
-    obstacle = new Array();
-    player = new PlayerCircle(canvas.width/2,canvas.height/2+ canvas.height*0.3,canvas.height*0.02,'#ffffff');
-    obstacle.push(new Obstacle(canvas.width/2,canvas.height/3,canvas.height*0.15,Math.floor(((Math.random()+1)*2)+0.5)));
-    maxScore = 0;
-    count = 0;
-    gamePaused = false;
-    player.draw();
-
-    canvas.addEventListener('click', play );
-    canvas.addEventListener('click', function (){
-        player.dy = 5;
-    });
+function resume() {
+    setTimeout(update,1000);
     pauseButton.addEventListener('click',pauseGame);
+    resumeImage.removeEventListener('click',resume);
+    reloadImage.removeEventListener('click',reload);
+    pauseScreen.style.display = "none";
+}
 
-for(let i=0;i<obstacle.length;i++)
-    obstacle[i].draw();
+function reload() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    gameEnd.style.display = "none";
+    pauseScreen.style.display = "none";
+    reloadImage.removeEventListener('click',reload);
+    resumeImage.removeEventListener('click',resume);
+    setup();
+}
+
+function pauseGame() {
+
+    pauseButton.removeEventListener('click',pauseGame);
+    clearInterval(render);
+
+    pauseScreen.appendChild(resumeImage);
+    pauseScreen.appendChild(reloadImage);
+    pauseScreen.style.display = "block";
+
+    resumeImage.addEventListener('click',resume);
+
+    reloadImage.addEventListener('click',reload);
+}
+
+function genBubbles() {
+
+    if(time < 400)
+        time = 350;
+    else
+        time -= index*10;
+
+    let r = Math.floor(Math.random()*10) + 3;
+    let x = Math.random()*(canvas.width - r*2) +r;
+    let y = Math.random()*(canvas.height - r*2) +r;
+    let c = 'white';
+
+    for(var j=0;j<balls.length;j++)
+    {
+        if(distance(x,y,balls[j].x,balls[j].y) < r + balls[j].r)
+        {
+            x = Math.random()*(canvas.width - r*2) +r;
+            y = Math.random()*(canvas.height - r*2) +r;
+        }
+        j = -1;
+    }
+
+    balls.push(new Circle(x,y,r,c));
+    index += 1;
+
+        // stop = setTimeout(genBubbles,time);
+        
+}
+
+function setup(){
+    
+
+    for(var i=0;i<20;i++)
+    {
+
+    let r = Math.floor(Math.random()*10) + 3;
+    let x = Math.random()*(canvas.width - r*2) +r;
+    let y = Math.random()*(canvas.height - r*2) +r;
+    let c = 'white';
+    
+    if(i!==0)
+    {
+        for(var j=0;j<balls.length;j++)
+        {
+            if(distance(x,y,balls[j].x,balls[j].y) < r + balls[j].r)
+            {
+                x = Math.random()*(canvas.width - r*2) +r;
+                y = Math.random()*(canvas.height - r*2) +r;
+            }
+            j = -1;
+        }
+    }
+
+    balls.push(new Circle(x,y,r,c));
+    balls[i].draw();
+    }
+
+    count = 0;
+    // genBubbles();
+    update();
 
 }
 
-function PlayerCircle (x,y,r,c) {
+function Circle(x,y,r,c){
     this.x = x;
     this.y = y;
     this.r = r;
     this.c = c;
+    this.mass = r*r;
 
-    this.dy;
-    this.g = 0.25;
-
-    this.draw = function() {
-        ctx.beginPath();
-        ctx.fillStyle = this.c;
-        ctx.arc (this.x,this.y,this.r,0,Math.PI*2);
-        ctx.fill();
-    }
-
-    this.jump = function() {
-        this.y -= this.dy;
-        this.dy -= this.g;
-        this.draw();
-
-        if(this.y<=canvas.height/2)
-        {
-            if(this.y <= canvas.height/3)
-            for(let i=0;i<obstacle.length;i++)
-            { 
-               obstacle[i].y += 1.5;
-                obstacle[i].draw();
-            }
-            else
-            for(let i=0;i<obstacle.length;i++)
-            { 
-               obstacle[i].y += 0.7;
-                obstacle[i].draw();
-            }
-           
-        }
-        for(let i=0;i<obstacle.length;i++)
-        {
-            if(this.y <= obstacle[i].y && this.y >= obstacle[i].y - obstacle[i].r)
-            {
-                if(maxScore<(i+1))
-                {
-                    count=i+1;
-                    maxScore=count;
-                    dispScore(maxScore);
-                    newObstacley = 3*canvas.height/5;
-                    obstacle.push(new Obstacle(canvas.width/2,obstacle[i].y - newObstacley,canvas.height*0.15,Math.floor(((Math.random()+1)*2)+0.5)));                   
-                }
-            }
-        
-        }
-
-    }
-
-}
-function Obstacle(x,y,r,n) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.startAngle = Math.PI/2;
-    this.endAngle;
-     
-    this.n = n;
-    
-    if( Math.random() - 0.5 >= 0)
-        this.clockAnti = 1;
-    else
-        this.clockAnti = -1;
+    this.dx = ((Math.random()*1)+0.1)*((Math.random()*1)-0.5)*2;
+    this.dy = ((Math.random()*1)+0.1)*((Math.random()*1)-0.5)*2; 
 
     this.draw = function () {
-     
-        for(var i=0;i<this.n;i++)
-        {
-            this.endAngle = this.startAngle + 2*Math.PI/n;
-            ctx.beginPath();
-            ctx.strokeStyle = color[i] ;
-            ctx.arc(this.x,this.y,this.r,this.startAngle,this.endAngle,false);
-            ctx.lineWidth=canvas.height/40;
-            this.startAngle = this.endAngle;
-            ctx.stroke();
-        }
+
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x,this.y,this.r, 0, Math.PI*2);
+        ctx.fill();
+
     }
 
-    this.rotate = function () {
-        this.startAngle += this.clockAnti*Math.PI/180;
+    this.animate = function () {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if ((this.x > canvas.width - this.r) || (this.x < this.r))
+            this.dx = -this.dx;
+        if ((this.y > canvas.height - this.r ) || (this.y < this.r))
+            this.dy = -this.dy;
+        
+        for(i=0;i<balls.length;i++)
+        {
+            if(this === balls[i])
+                continue;
+            else if ( distance(this.x,this.y,balls[i].x,balls[i].y) <= this.r + balls[i].r  )
+            { 
+                // resolveCollision(this.balls[i]);  
+             }
+        }    
         this.draw();
     }
-    
-    this.burst = function () {
 
-        if((player.y - this.y <= (player.r + this.r + canvas.height/80)) && (player.y - this.y >= (this.r - player.r - canvas.height/80)))
-        {
-            pixel = ctx.getImageData(this.x,this.y + this.r,1,1).data;
-            if(!(pixel[0]==255&&pixel[1]==255&&pixel[2]==255))
-              {  ctx.clearRect(0,0,canvas.width,canvas.height);
-                clearInterval(render);
+    this.click = function (event) {
+        if( distance(event.clientX,event.clientY,this.x,this.y) <= this.r + 1)
+            for(var i =0;i<balls.length;i++)
+                if(this == balls[i])
+                {
+                    balls.splice(i,1);
                     burstAudio();
-                canvas.removeEventListener('click',jumpAudio);
-                for(var j=0;j<obstacle.length;j++)
-                {
-                    obstacle[j].draw();
-                }
-              }
-
-        }
-
-        else if((this.y - player.y <= (player.r + this.r + canvas.height/80)) && (this.y - player.y >= (this.r - player.r - canvas.height/80)))
-        {
-            pixel = ctx.getImageData(this.x,this.y - this.r,1,1).data;
-            if(!(pixel[0]==255&&pixel[1]==255&&pixel[2]==255))
-              {  ctx.clearRect(0,0,canvas.width,canvas.height);
-                clearInterval(render);                
-                burstAudio();
-                canvas.removeEventListener('click',jumpAudio);
-                for(var j=0;j<obstacle.length;j++)
-                {
-                    obstacle[j].draw();
-                }
-              }
-        }
-        else if (player.y>canvas.height)
-        {
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-                clearInterval(render);                
-                burstAudio();
-                canvas.removeEventListener('click',jumpAudio);
-                for(var j=0;j<obstacle.length;j++)
-                {
-                    obstacle[j].draw();
+                    count ++;
+                    dispScore(count);
                 }
         }
-    }
 
 }
 
-function play () {
-    canvas.removeEventListener('click',play);
-    pauseButton.addEventListener('click',pauseGame);
+    function burst() {
+        for(var i=0;i<balls.length;i++)
+            balls[i].click(event);
+    }
 
-         render = setInterval(function(){
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            player.jump();
-        for(let i=0;i<obstacle.length;i++)
-        {    obstacle[i].rotate();
-                obstacle[i].burst();
-        }
-    },16);
-
-} 
+function update(){
+   
+//    render = setInterval( function(){
+//        ctx.clearRect(0,0,canvas.width,canvas.height);
+//     //    areaCheck();
+//     for (var i=0;i<balls.length;i++)
+//         balls[i].animate();
+//    },16);
+}
